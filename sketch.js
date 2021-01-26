c0c998287b99d12ac9f3ba8f771ba71130270669
ParadoxFlame <70868901+ParadoxFlame@users.noreply.github.com>
2021-01-25 17:20:37 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -581,9 +581,9 @@ function draw() {
   drawSprites();
   fill("white");
   textSize(20);
-  text("Score: " + score, camera.position.x - windowWidth/4, camera.position.y - windowHeight/3);
+  text("Score: " + score, camera.position.x - windowWidth/5, camera.position.y - windowHeight/6);
 
-  text("Level: " + gameState, camera.position.x - windowWidth/4, camera.position.y - windowHeight/3 + 25);
+  text("Level: " + gameState, camera.position.x - windowWidth/5, camera.position.y - windowHeight/6 + 25);
 
   if(gameState === "cutscene1")
   {
